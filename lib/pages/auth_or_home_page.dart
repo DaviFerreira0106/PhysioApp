01ce7e39739530aa2e7f44695861a5f796fd3a69
Davi Ferreira <daviferreira0106@gmail.com>
2025-05-23 21:46:25 -0300

    Implementação da tela home e navigationbottom

--- lib/pages/auth_or_home_page.dart
+++ lib/pages/auth_or_home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:physioapp/pages/auth_page.dart';
-import 'package:physioapp/pages/home_page.dart';
+import 'package:physioapp/pages/tabs_page.dart';
 import 'package:provider/provider.dart';
 import 'package:physioapp/controller/auth_controller.dart';
 
@@ -17,6 +17,6 @@ class ControllerPageState extends State<AuthOrHomePage> {
   @override
   Widget build(BuildContext context) {
     final AuthController auth = Provider.of(context);
-    return auth.isAuth ? HomePage() : const AuthPage();
+    return auth.isAuth ? const TabsPage() : const AuthPage();
   }
 }
